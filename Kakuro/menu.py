e30b9cf097f8edd32f7a67889813d0b5a68414f3
Ewa55555 <ecia.jedi@gmail.com>
2016-12-11 18:42:56 +0100

    Possibility of writing numbers into node from keyboard Display Sum of column

--- Kakuro/menu.py
+++ Kakuro/menu.py
@@ -2,77 +2,60 @@ import random
 from pygame import font
 from Kakuro.node import *
 from Kakuro.display import *
-pygame.init()
+from Kakuro.button import *
+from Kakuro.settings import *
+from Kakuro.Sum_of_column import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.nodes = list()
+
+    def gameloop(self):
+        while True:
+            gameDisplay.fill(white)
+            for node in self.nodes:
+                node.show()
+            suma = Sum_of_column([100, 60], [100, 100], [140, 100], 10)
+            suma.show()
+            pygame.draw.polygon(gameDisplay, black, [[100, 60], [100, 100], [140, 100]], 1)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: #Quit to zamkniecie okna przez uzytkownika
+                    pygame.quit()
+                    quit()
+                for node in self.nodes:
+                    node.update(event, gameDisplay)
+            pygame.display.update()
+            clock.tick(60)
 
 
+    def menu(self):
+        intro = True
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
 
+            gameDisplay.fill(white)
+            position = ((width / 2), (height / 3))
+            textDisplay("Kakuro", 100, black, position)
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+            buttonPlay = Button(350, 300, 100, 50, green, "Graj", 30)
+            buttonPlay.show()
+            if buttonPlay.backlight(mouse):
+                if(buttonPlay.isClicked()):
+                    gameDisplay.fill(white)
+                    kwadracik = Node(1, 100, 100)
+                    self.nodes.append(kwadracik)
+                    self.gameloop()
 
-def gameloop():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: #Quit to zamkniecie okna przez uzytkownika
-                pygame.quit()
-                quit()
-        gameDisplay.fill(white)
-        textDisplay("Let's game begin...", 40, red, (width/2, height/2))
-        kwadracik = Node(1, 20, 30)
-        kwadracik.show(gameDisplay)
-        pygame.display.update()
-        clock.tick(60)
+            pygame.display.update()
+            clock.tick(15)
 
+    def start(self):
+        self.menu()
 
-class Button:
-    def __init__(self,xleft, yleft, width, height, color, text, sizeOfLetters):
-        self.x = xleft
-        self.y = yleft
-        self.w = width
-        self.h = height
-        self.color = color
-        self.text = text
-        self.size = sizeOfLetters
-
-    def show(self):
-        pygame.draw.rect(gameDisplay, self.color, (self.x, self.y, self.w, self.h))
-        self.positionOfText = ((self.x + (self.w / 2)), (self.y + (self.h / 2)))
-        textDisplay(self.text, self.size, black, self.positionOfText)
-
-    def changeColor(self,color):
-        self.color = color
-
-    def backlight(self, mouse):
-        if self.x + self.w > mouse[0] > self.x and self.y + self.h > mouse[1] > self.y:
-            self.changeColor(bright_green)
-            self.show()
-
-    def isClicked(self):
-        click = pygame.mouse.get_pressed()
-        if click[0] == 1:
-            return True
-        else:
-            return False
-
-
-def menu():
-    intro = True
-    while intro:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        gameDisplay.fill(white)
-        position = ((width / 2), (height / 3))
-        textDisplay("Kakuro", 100, black, position)
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-        buttonPlay = Button(350, 300, 100, 50, green, "Graj", 30)
-        buttonPlay.show()
-        buttonPlay.backlight(mouse)
-        if(buttonPlay.isClicked()):
-            gameloop()
-
-
-        pygame.display.update()
-        clock.tick(15)
-
-menu()
+game = Game()
+game.start()
--- Kakuro/node.py
+++ Kakuro/node.py
@@ -12,27 +12,38 @@ class Node:
         self.height = 40
         self.width = 40
         self.rect = pygame.Rect(self.left, self.top, self.width, self.height)
+        self.selected = False
+        self.list_of_numbers = {pygame.K_1: 1, pygame.K_2: 2, pygame.K_3: 3,pygame.K_4: 4, pygame.K_5: 5, pygame.K_6: 6,
+                                pygame.K_7: 7, pygame.K_8: 8, pygame.K_9: 9}
 
-    def show(self, surface):
-        pygame.draw.rect(surface, self.color, self.rect, 1)
-        t = textObject(str(self.number), 20, self.color)
+    def show(self):
+        if self.selected:
+            pygame.draw.rect(gameDisplay, self.color, self.rect, 2)
+        else:
+            pygame.draw.rect(gameDisplay, self.color, self.rect, 1)
+        t = textObject(str(self.number), 25, self.color)
         rect = t.get_rect()
         rect.center = (self.left + (self.width/2), self.top + (self.height/2))
         gameDisplay.blit(t, rect)
 
     def add_number(self, event):
-        char = event.unicode
-        if char:  # stop emtpy space for shift key adding to list
-            self.str_list.append(char)
+        if event.key == pygame.K_BACKSPACE:
+            self.number = ""
+        elif event.key in self.list_of_numbers.keys():
+           self.number = self.list_of_numbers[event.key]
 
-    def is_selected(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
+    def update(self, event, surface):
+        if event.type == pygame.KEYDOWN:
+            if self.selected:
+                input_entered = self.add_number(event)
+                if input_entered:
+                    print(input_entered)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if self.rect.collidepoint(pygame.mouse.get_pos()):
-                    return True
-            else:
-                    return False
-
+                    self.selected = True
+                else:
+                    self.selected = False
 
 
 
