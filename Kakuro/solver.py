b83dfe6f4f0739171f981af6e70635babc4851bf
Ewa55555 <ecia.jedi@gmail.com>
2017-01-03 19:40:48 +0100

    changed factorising algorythm

--- Kakuro/solver.py
+++ Kakuro/solver.py
@@ -5,6 +5,7 @@ from Kakuro.button import *
 from Kakuro.settings import *
 from Kakuro.SumOfColumn import *
 from Kakuro.Board import *
+import copy
 
 
 class Solver:
@@ -32,6 +33,35 @@ class Solver:
         for e in factorisation:
             print(e)
 
+    def f(self, number, count):
+        available_numbers = list(range(1, 10))
+        self.factor(number, count, available_numbers)
+
+    def factor(self, number, count, available_numbers):
+        if count == 1:
+            l = list()
+            if number in available_numbers:
+                available_numbers.remove(number)
+                print(number)
+                l.append(number)
+            return l
+        else:
+            available_numbers_copy = copy.copy(available_numbers)
+            list_of_all = list()
+            for i in reversed(available_numbers_copy):
+                if i < number and i in available_numbers:
+                    temp_number = number - i
+                    available_numbers.remove(i)
+                    l = self.factor(temp_number, count - 1, available_numbers)
+                    if l:
+                        print(i)
+                        l.append(i)
+                        list_of_all.append(l)
+
+
+
+
+
     def game(self):
         gameDisplay.fill(white)
         self.board.generate2()
@@ -54,4 +84,5 @@ class Solver:
 
 
 s = Solver()
-s.game()
+s.f(10, 3)
+
