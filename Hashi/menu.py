d0256814a1ade4af29918995f75eba197e526f5e
Kasia <kasia.haja@gmail.com>
2017-01-07 16:13:05 +0100

    add hints and show all bridge

--- Hashi/menu.py
+++ Hashi/menu.py
@@ -100,7 +100,12 @@ def choose_level():
 def gameloop(g):
     pygame.display.update()
     clock.tick(15)
+    is_show = False
+    is_hint = False
+    bridge = list()
+
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -112,11 +117,21 @@ def gameloop(g):
         gameDisplay.fill(blur_violet)
         r = pygame.Rect(0, 0, 600, 500)
         sub = gameDisplay.subsurface(r)
-        button_save = Button(650, 380, 100, 50, violet, "Zapisz", 30, 1)
+        button_save = Button(650, 380, 120, 50, violet, "Zapisz", 25, 1)
         button_save.show()
         button_save.backlight(mouse)
+        button_solve = Button(650, 280, 120, 50, violet, "Rozwiązanie", 25, 1)
+        button_solve.show()
+        button_solve.backlight(mouse)
+        button_hint = Button(650, 180, 120, 50, violet, "Wskazówka", 25, 1)
+        button_hint.show()
+        button_hint.backlight(mouse)
         g.board.generate_board()
-        g.board.print_bridge()
+        if is_show ==True:
+            g.board.print_bridge()
+
+
+
         if button_save.isClicked(mouse):
             a = datetime.datetime(2013, 12, 30, 23, 59, 59)
             b = datetime.datetime.now()
@@ -126,6 +141,21 @@ def gameloop(g):
             textDisplay("Zapisano do pliku", 30, dark_violet, position)
             pygame.display.update()
             pygame.image.save(sub, "generated_boards/"+fname+".png")
+        if button_solve.isClicked(mouse):
+            is_show = True
+        if button_hint.isClicked(mouse):
+            is_hint = True
+            g.number_of_hints += 1
+            bridge.append(g.random_bridge())
+        if is_hint == True:
+            for i in bridge:
+                if i.number == 1:
+                    i.show()
+                if i.number == 2:
+                    i.show_more()
+
+
+
 
         pygame.display.update()
         clock.tick(15)
