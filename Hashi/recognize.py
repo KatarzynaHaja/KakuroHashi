22c3d227f27df1e402103f65f0b3d902ada93cb0
KatarzynaHaja <kasia.haja@gmail.com>
2017-01-09 13:20:58 +0100

    add files

--- Hashi/recognize.py
+++ Hashi/recognize.py
@@ -7,6 +7,7 @@ cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,50,param1=100,param2=15,minRadius=10,maxRadius=30)
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
+    print(i[0])
     cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
     cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 cv2.imshow('detected',cimg)
