5686a7413b0cfc04e345e4adcb6a46275c24b161
KatarzynaHaja <kasia.haja@gmail.com>
2017-02-01 15:35:13 +0100

    docstrings and refactoring

--- Hashi/bridge.py
+++ Hashi/bridge.py
@@ -1,49 +1,62 @@
-import pygame
 from Hashi.display import *
-class Bridge():
-    def __init__(self, fromCircle, toCircle , color,value):
-        self.circle1 = fromCircle
-        self.circle2 = toCircle
+
+
+class Bridge:
+    def __init__(self, from_circle, to_circle, color, value):
+        """
+        Constructor
+        :param from_circle: source circle
+        :param to_circle: destination circle
+        :param color: bridge's color
+        :param value: it says how many bridges connect 2 circles
+        """
+        self.circle1 = from_circle
+        self.circle2 = to_circle
         self.color = color
         self.number = value
 
     def show(self):
-        if self.circle1.x  == self.circle2.x:
+        """
+        This function draws bridge between two circles.
+        """
+        if self.circle1.x == self.circle2.x:
             if self.circle1.y > self.circle2.y:
-                pygame.draw.line(gameDisplay, self.color,(self.circle1.x, self.circle1.y - self.circle1.r), (self.circle2.x , self.circle2.y + self.circle2.r))
+                pygame.draw.line(gameDisplay, self.color, (self.circle1.x, self.circle1.y - self.circle1.r),
+                                 (self.circle2.x, self.circle2.y + self.circle2.r))
             else:
-                pygame.draw.line(gameDisplay, self.color, (self.circle2.x, self.circle2.y - self.circle2.r),(self.circle1.x, self.circle1.y + self.circle1.r))
+                pygame.draw.line(gameDisplay, self.color, (self.circle2.x, self.circle2.y - self.circle2.r),
+                                 (self.circle1.x, self.circle1.y + self.circle1.r))
         if self.circle1.y == self.circle2.y:
             if self.circle1.x > self.circle2.x:
-                pygame.draw.line(gameDisplay, self.color, (self.circle1.x - self.circle1.r, self.circle1.y),(self.circle2.x + self.circle2.r, self.circle2.y))
+                pygame.draw.line(gameDisplay, self.color, (self.circle1.x - self.circle1.r, self.circle1.y),
+                                 (self.circle2.x + self.circle2.r, self.circle2.y))
             else:
-                pygame.draw.line(gameDisplay, self.color, (self.circle2.x - self.circle2.r, self.circle2.y),(self.circle1.x + self.circle1.r, self.circle1.y))
-
+                pygame.draw.line(gameDisplay, self.color, (self.circle2.x - self.circle2.r, self.circle2.y),
+                                 (self.circle1.x + self.circle1.r, self.circle1.y))
 
     def show_more(self):
-            if self.circle1.x == self.circle2.x:
-                if self.circle1.y > self.circle2.y:
-                    pygame.draw.line(gameDisplay, self.color, (self.circle1.x-10, self.circle1.y - self.circle1.r),
-                                     (self.circle2.x - 10, self.circle2.y + self.circle2.r))
-                    pygame.draw.line(gameDisplay, self.color, (self.circle1.x + 10, self.circle1.y - self.circle1.r),
-                                     (self.circle2.x + 10, self.circle2.y + self.circle2.r))
-                else:
-                    pygame.draw.line(gameDisplay, self.color, (self.circle2.x - 10, self.circle2.y - self.circle2.r),
-                                     (self.circle1.x - 10, self.circle1.y + self.circle1.r))
-                    pygame.draw.line(gameDisplay, self.color, (self.circle2.x + 10, self.circle2.y - self.circle2.r),
-                                     (self.circle1.x + 10, self.circle1.y + self.circle1.r))
-            if self.circle1.y == self.circle2.y:
-                if self.circle1.x > self.circle2.x:
-                    pygame.draw.line(gameDisplay, self.color, (self.circle1.x - self.circle1.r, self.circle1.y - 10),
-                                     (self.circle2.x + self.circle2.r, self.circle2.y - 10))
-                    pygame.draw.line(gameDisplay, self.color, (self.circle1.x - self.circle1.r, self.circle1.y + 10),
-                                     (self.circle2.x + self.circle2.r, self.circle2.y + 10))
-                else:
-                    pygame.draw.line(gameDisplay, self.color, (self.circle2.x - self.circle2.r, self.circle2.y - 10),
-                                     (self.circle1.x + self.circle1.r, self.circle1.y - 10))
-                    pygame.draw.line(gameDisplay, self.color, (self.circle2.x - self.circle2.r, self.circle2.y +10),
-                                     (self.circle1.x + self.circle1.r, self.circle1.y +10))
-
-
-
-
+        """
+        Function which draws 2 bridges between two circle
+        """
+        if self.circle1.x == self.circle2.x:
+            if self.circle1.y > self.circle2.y:
+                pygame.draw.line(gameDisplay, self.color, (self.circle1.x - 10, self.circle1.y - self.circle1.r),
+                                 (self.circle2.x - 10, self.circle2.y + self.circle2.r))
+                pygame.draw.line(gameDisplay, self.color, (self.circle1.x + 10, self.circle1.y - self.circle1.r),
+                                 (self.circle2.x + 10, self.circle2.y + self.circle2.r))
+            else:
+                pygame.draw.line(gameDisplay, self.color, (self.circle2.x - 10, self.circle2.y - self.circle2.r),
+                                 (self.circle1.x - 10, self.circle1.y + self.circle1.r))
+                pygame.draw.line(gameDisplay, self.color, (self.circle2.x + 10, self.circle2.y - self.circle2.r),
+                                 (self.circle1.x + 10, self.circle1.y + self.circle1.r))
+        if self.circle1.y == self.circle2.y:
+            if self.circle1.x > self.circle2.x:
+                pygame.draw.line(gameDisplay, self.color, (self.circle1.x - self.circle1.r, self.circle1.y - 10),
+                                 (self.circle2.x + self.circle2.r, self.circle2.y - 10))
+                pygame.draw.line(gameDisplay, self.color, (self.circle1.x - self.circle1.r, self.circle1.y + 10),
+                                 (self.circle2.x + self.circle2.r, self.circle2.y + 10))
+            else:
+                pygame.draw.line(gameDisplay, self.color, (self.circle2.x - self.circle2.r, self.circle2.y - 10),
+                                 (self.circle1.x + self.circle1.r, self.circle1.y - 10))
+                pygame.draw.line(gameDisplay, self.color, (self.circle2.x - self.circle2.r, self.circle2.y + 10),
+                                 (self.circle1.x + self.circle1.r, self.circle1.y + 10))
--- Hashi/button.py
+++ Hashi/button.py
@@ -1,35 +1,53 @@
-import pygame
-from Hashi.settings import *
 from Hashi.display import *
 
 
 class Button:
-    def __init__(self,xleft, yleft, width, height, color, text, sizeOfLetters,i):
+    def __init__(self, xleft, yleft, width, height, color, text, size_of_letters, i):
         self.x = xleft
         self.y = yleft
         self.w = width
         self.h = height
         self.color = color
         self.text = text
-        self.size = sizeOfLetters
-        self.is_cliked = False
+        self.size = size_of_letters
+        self.is_clicked = False
         self.position = i
 
     def show(self):
+        """
+        This function is responsible to show button on the screen
+        :return:
+        """
         pygame.draw.rect(gameDisplay, self.color, (self.x, self.y, self.w, self.h))
-        self.positionOfText = ((self.x + (self.w / 2)), (self.y + (self.h / 2)))
-        textDisplay(self.text, self.size, dark_violet, self.positionOfText)
+        self.position_of_text = ((self.x + (self.w / 2)), (self.y + (self.h / 2)))
+        textDisplay(self.text, self.size, dark_violet, self.position_of_text)
 
-    def changeColor(self,color):
+    def change_color(self, color):
+        """
+        This method change a color of button
+        :param color: name of color
+        :return:
+        """
         self.color = color
 
     def backlight(self, mouse):
+        """
+        This method highlight button when position of mouse is in button, square
+        :param mouse:Position of mouse
+        :return:
+        """
         if self.x + self.w > mouse[0] > self.x and self.y + self.h > mouse[1] > self.y:
-            self.changeColor(bright_violet)
+            self.change_color(bright_violet)
             self.show()
 
     def isClicked(self, mouse):
-        if pygame.event.peek(pygame.MOUSEBUTTONDOWN) and self.x + self.w > mouse[0] > self.x and self.y + self.h > mouse[1] > self.y:
+        """
+        This function says if button is clicked. Peek of the queue is checked
+        :param mouse: position of mouse
+        :return: logical value : yes if button is clicked and no if not
+        """
+        if pygame.event.peek(pygame.MOUSEBUTTONDOWN) and self.x + self.w > mouse[0] > self.x and self.y + self.h > \
+                mouse[1] > self.y:
             pygame.event.clear()
             return True
         else:
