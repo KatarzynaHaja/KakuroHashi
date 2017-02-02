947a2e27e57cf792d5a5239430c0dfe6aa098043
KatarzynaHaja <kasia.haja@gmail.com>
2017-02-02 20:14:18 +0100

    unit tests

--- Hashi/test/test_board.py
+++ Hashi/test/test_board.py
@@ -1,15 +1,16 @@
 import random
 import unittest
-import Hashi
+from Hashi.board import *
+from operator import attrgetter
 
 
 class TestBoard(unittest.TestCase):
-    def setUp(self):
-        self.board = Hashi.board.Board(4)
 
-    def test_generate_default_board(self):
-        self.assertTrue(len(self.board.generate_default_board()) == 30)
-
+    def test_sort_circle(self):
+        self.circles = list()
+        for i in range(5):
+            self.circles.append(Circle(2,500 -i*20,100,violet))
+        self.assertEqual(sort_circle(self.circles,'x'),sorted(self.circles, key=attrgetter('x')))
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestBoard)
 print(unittest.TextTestRunner(verbosity=3).run(suite))
